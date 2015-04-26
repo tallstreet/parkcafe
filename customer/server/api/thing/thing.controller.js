984052d9005ce02838d5d8efedc4df3d01e6003d
Rob Finean <rfinean@iee.org>
2015-04-26 03:40:06 +0100

    add product ids

--- customer/server/api/thing/thing.controller.js
+++ customer/server/api/thing/thing.controller.js
@@ -16,15 +16,19 @@ exports.index = function(req, res) {
   res.json([
   {
   name : 'Ice Cream',
+  id : 'icecream',
   image : 'assets/images/Bitmap 1@2x.png'
   }, {
   name : 'Coffee',
+  id : 'coffee',
   image : 'assets/images/Bitmap 2@2x.png'
   }, {
   name : 'Roast Chicken',
+  id : 'chicken',
   image : 'assets/images/Bitmap 3@2x.png'
   },  {
   name : 'Chilled Beer',
+  id : 'beer',
   image : 'assets/images/Bitmap 4@2x.png'
   }
   ]);
